54033cf64f113d2f11a100dc6d88763161fec9de
Sana-Farasat <naz62354@gmail.com>
2025-02-10 01:38:56 +0500

    updated

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import productSchema from './product'
+import product from './product'
+//import productSchema from './product'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema],
+  //types: [productSchema],
+   types: [product]
 }
